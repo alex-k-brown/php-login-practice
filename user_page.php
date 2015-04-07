148ec0b17b98d120262fe498a4772fe10adde564
Alex Brown <brown.alex.k@gmail.com>
2015-04-07 17:56:29 -0600

    made user page, got data from database showing up in a table, formatted table

--- user_page.php
+++ user_page.php
@@ -1,10 +1,58 @@
 <!DOCTYPE html>
 
+<?php
+
+    // mySQLi
+
+    $mysqli = new mysqli("localhost", "root", "", "login_practice");
+
+    //  SQL Statement
+
+    $sql = 'SELECT * FROM `users` WHERE 1';
+
+//    $update_name = 'UPDATE users SET username="'.$_POST["new-name"].'" WHERE id=2';
+//    $update_pass = 'UPDATE users SET username="'.$_POST["new-pass"].'" WHERE id=2';
+
+    //  mySQL Query
+
+    $result = $mysqli->query($sql);
+
+    //  Retrieve Data
+
+
+    //  Close connection
+    $mysqli->close();
+
+?>
+
 <html>
   <head>
     <title>User Page</title>
+    <link href="style.css" type="text/css" rel="stylesheet"/>
   </head>
   <body>
-    <p>Hello, World!</p>
+
+      <table>
+        <tr>
+          <th>ID</th>
+          <th>Username</th>
+          <th>Password</th>
+          <th></th>
+          <th></th>
+        </tr>
+        <?php
+          while($row = $result->fetch_assoc()){
+            echo "<tr><td>";
+            print_r($row["id"]);
+            echo '</td><td>';
+            print_r($row["username"]);
+            echo  '<form action="" method="post"><input class="update" type="submit" value=""><input class="new-name" type="text" name="new-name"></form></td><td>';
+            print_r($row["password"]);
+            echo '<form action="" method="post"><input class="update" type="submit" value=""><input class="new-pass" type="text" name="new-pass"></form></td>
+                  <td><input type="submit" value="Delete"></td>
+                  </tr>';
+          }
+        ?>
+      </table>
   </body>
 </html>
